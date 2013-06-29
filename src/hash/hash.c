66f08ebeca85286c98805f7cd8e56067214f9574
¨crfwilliams¨ <crfwilliams@gmail.com>
2013-06-28 23:10:14 -0400

    [src] Inclusao da primeira versao da hash

---
+++ src/hash/hash.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "uthash.h"
+
+typedef struct {
+  char *nome;
+  char *escopo;
+} item_key;
+
+typedef struct {
+    item_key key;
+    char *tipo;
+    UT_hash_handle hh;
+} item;
+
+item l, *p,  *tmp, *records = NULL;
+
+void hash_inserir(char *nome, char *escopo, char *tipo)
+{
+    item *r;
+    r = (item*)malloc( sizeof(item) );
+    memset(r, 0, sizeof(item));
+    r->key.nome = nome;
+    r->key.escopo = escopo;
+    r->tipo = tipo;
+    HASH_ADD(hh, records, key, sizeof(item_key), r); 
+}
+
+struct item *hash_consultar(char *nome, char *escopo)
+{
+	
+    memset(&l, 0, sizeof(item));
+    l.key.nome = nome;
+    l.key.escopo = escopo;
+    HASH_FIND(hh, records, &l.key, sizeof(item_key), p);
+
+    if (p) 
+    {	
+	printf("encontrado: %s %s %s\n", p->key.nome, p->key.escopo, p->tipo);
+	return p;
+    }
+}
+
+
+
+
+int main(int argc, char *argv[]) {
+    
+    
+    hash_inserir("nome1", "escopo1", "tipo1");    
+
+    hash_consultar("nome1", "escopo1");	
+
+    return 0;
+}
